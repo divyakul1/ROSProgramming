6315b068f2ed5b9d49b6618911f372cfb243889d
divyakul1 <dpkulkar@andrew.cmu.edu>
2017-10-02 20:54:58 -0400

    publish and subsribing done

--- src/chatbot_node/src/chatbot_node.cpp
+++ src/chatbot_node/src/chatbot_node.cpp
@@ -3,8 +3,18 @@
 #include <message_ui/sent_msg.h>
 #include <string>
 
+std::string replymessage ("I didn't get that");
+
+
+
 using namespace std;
 
+
+//ros::NodeHandle n;
+
+ros::Publisher pub;
+
+//ros::Publisher pub = n.advertise<chatbot_node::reply_msg>("reply_msg", 1000);
 
 //Add your code here
 
@@ -13,31 +23,61 @@ using namespace std;
 void sent_msg_callback(const message_ui::sent_msg mess)
 {
 	//message=sent_msg.message;
-  std::cout << "I heard" << std::endl;
-  std::cout << mess.message<< std::endl;
+  //std::cout << "I heard" << std::endl;
+  //std::cout << mess.message<< std::endl;
 	//cout<< message.message;
+
+  if (mess.message.compare("Hello") == 0) {
+
+    replymessage = "Hello Divya";
+    //std::cout << replymessage << '\n';
+  }
+
+  else if (mess.message.compare("What is your name?")==0) {
+
+    replymessage = "My name is MRSD Siri";
+    //std::cout << replymessage << '\n';
+  }
+
+  else if (mess.message.compare("How are you?")==0)
+  {
+    replymessage = "I am fine, thank you";
+    //std::cout << replymessage << '\n';
+  }
+
+  else{
+
+    replymessage = "I didn't get that";
+    //std::cout << replymessage << '\n';
+
+  }
+
+
+  chatbot_node::reply_msg response;
+  response.message = replymessage;
+
+  pub.publish(response);
+
 
 }
 
 int main(int argc, char **argv) {
 
-  ros::init(argc, argv, "chatbot_node");
+  ros::init(argc, argv, "chatbot node");
+
   ros::NodeHandle n;
 
+  pub =n.advertise<chatbot_node::reply_msg>("reply_msg", 1000);
   ros::Subscriber sub = n.subscribe("sent_msg", 1000, sent_msg_callback);
-  
-
-  //ros::Publisher reply_msg_pub = n.advertise<reply_msg.msg>("reply_msg",1000);
-
-  //Add your code here
-
 
 
   ros::Rate loop_rate(20);
 
   while(ros::ok()) {
+
     ros::spinOnce();
     loop_rate.sleep();
+
   }
 
   return 0;
